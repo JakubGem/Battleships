3eaca4a3eccda0b63bc6acb500cdafa61a27f608
JakubGem <jakegemb89@gmail.com>
2022-03-09 17:26:58 +0100

    i have no idea why validation is not working

--- src/Board.java
+++ src/Board.java
@@ -3,10 +3,12 @@ import java.util.List;
 
 public class Board {
 
-
     public List<List<Square>> ocean = new ArrayList<>();
 
+    private static boolean isPlacementOk(int[][] placement) {
 
+        return false;
+    }
 
     public Board() {
         for (int j = 0; j < 15; j++) {
@@ -16,28 +18,127 @@ public class Board {
             }
             ocean.add(row);
         }
-//        ocean.get(0).get(4).setStatusSquare("D");
-//        ocean.get(0).get(3).setStatusSquare("D");
-//        ocean.get(0).get(2).setStatusSquare("D");
-//        ocean.get(5).get(2).setStatusSquare("M");
-//        ocean.get(7).get(12).setStatusSquare("M");
-//        ocean.get(13).get(8).setStatusSquare("H");
-//        ocean.get(13).get(9).setStatusSquare("H");
-//        ocean.get(13).get(10).setStatusSquare("S");
-//        ocean.get(2).get(9).setStatusSquare("S");
-//        ocean.get(3).get(9).setStatusSquare("S");
-//        ocean.get(4).get(9).setStatusSquare("S");
-//        ocean.get(5).get(9).setStatusSquare("S");
-//        ocean.get(6).get(9).setStatusSquare("S");
+    }
 
+    public static int[] carrierPlacement(Board board) {
+        int right = 0;
+        int down = 0;
+
+        int[] result = BoardFactory.random(board);
+
+        try {
+            if (board.ocean.get(result[0]).get(result[1]).getStatusSquare().equals("O") && board.ocean.get(result[0]).get(result[1] + 1).getStatusSquare().equals("O")
+                    && board.ocean.get(result[0]).get(result[1] + 2).getStatusSquare().equals("O") && board.ocean.get(result[0]).get(result[1] + 3).getStatusSquare().equals("O")
+                    && board.ocean.get(result[0]).get(result[1] + 4).getStatusSquare().equals("O")) {
+                    right += 1;
+                System.out.println("1try");
+//            board.ocean.get(result[0]).get(result[1] + d).setStatusSquare("S");
+            }
+        } catch (IndexOutOfBoundsException e) {
+            try {
+                if
+                (board.ocean.get(result[0]).get(result[1]).getStatusSquare().equals("O") && board.ocean.get(result[0] + 1).get(result[1]).getStatusSquare().equals("O")
+                        && board.ocean.get(result[0] + 2).get(result[1]).getStatusSquare().equals("O") && board.ocean.get(result[0] + 3).get(result[1]).getStatusSquare().equals("O")
+                        && board.ocean.get(result[0] + 4).get(result[1]).getStatusSquare().equals("O")) {
+                    down = 1;
+                    System.out.println("2try");
+                }
+            } catch (IndexOutOfBoundsException event) {
+                for (int d = 0; d < board.ocean.size()-1; d++) {
+                    board.ocean.get(result[0] + d).get(result[1]).setStatusSquare("O");
+                    board.ocean.get(result[0]).get(result[1] + d).setStatusSquare("O");
+                    System.out.println("carrier placement nie działa");
+                    carrierPlacement(board);
+                }
+                System.out.println("dupa1");
+            }
+        }int[] resultVar = {right, down, result[0], result[1]};
+        return resultVar;
+    }
+
+    public static boolean battleshipPlacement(boolean isBattleshipPlacementOk, Board board) {
+        int[] result = BoardFactory.random(board);
+        if (board.ocean.get(result[0]).get(result[1]).getStatusSquare().equals("O") && board.ocean.get(result[0] + 1).get(result[1]).getStatusSquare().equals("O")
+                && board.ocean.get(result[0] + 2).get(result[1]).getStatusSquare().equals("O") && board.ocean.get(result[0] + 3).get(result[1]).getStatusSquare().equals("O")) {
+            for (int d = 0; d < 4; d++) {
+                board.ocean.get(result[0]).get(result[1] + d).setStatusSquare("S");
+            }
+        } else if (board.ocean.get(result[0]).get(result[1]).getStatusSquare().equals("O") && board.ocean.get(result[0]).get(result[1] + 1).getStatusSquare().equals("O")
+                && board.ocean.get(result[0]).get(result[1] + 2).getStatusSquare().equals("O") && board.ocean.get(result[0]).get(result[1] + 3).getStatusSquare().equals("O")) {
+            for (int d = 0; d < 4; d++) {
+                board.ocean.get(result[0] + d).get(result[1]).setStatusSquare("S");
+            }
+        } else {
+            for (int d = 0; d < 4; d++) {
+                board.ocean.get(result[0] + d).get(result[1]).setStatusSquare("O");
+                board.ocean.get(result[0]).get(result[1 + d]).setStatusSquare("O");
+                battleshipPlacement(isBattleshipPlacementOk, board);
+            }
+        }
+
+        return isBattleshipPlacementOk;
+    }
+
+    public static boolean CruiserPlacement(boolean isCruiserPlacementOk, Board board) {
+
+        int[] result = BoardFactory.random(board);
+        if (board.ocean.get(result[0]).get(result[1]).getStatusSquare().equals("O") && board.ocean.get(result[0] + 1).get(result[1]).getStatusSquare().equals("O")
+                && board.ocean.get(result[0] + 2).get(result[1]).getStatusSquare().equals("O")) {
+            for (int d = 0; d < 3; d++) {
+                board.ocean.get(result[0] + d).get(result[1]).setStatusSquare("S");
+            }
+        } else if (board.ocean.get(result[0]).get(result[1]).getStatusSquare().equals("O") && board.ocean.get(result[0]).get(result[1] + 1).getStatusSquare().equals("O")
+                && board.ocean.get(result[0]).get(result[1] + 2).getStatusSquare().equals("O")) {
+            for (int d = 0; d < 3; d++) {
+                board.ocean.get(result[0]).get(result[1] + d).setStatusSquare("S");
+            }
+        } else {
+            for (int d = 0; d < 3; d++) {
+                board.ocean.get(result[0] + d).get(result[1]).setStatusSquare("O");
+                board.ocean.get(result[0]).get(result[1] + d).setStatusSquare("O");
+                CruiserPlacement(isCruiserPlacementOk, board);
+            }
+
+        }
+        return isCruiserPlacementOk;
+    }
+
+    public static boolean SubmarinePlacement(boolean isSubmarinePlacementOk, Board board) {
+        int[] result = BoardFactory.random(board);
+        if (board.ocean.get(result[0]).get(result[1]).getStatusSquare().equals("O") && board.ocean.get(result[0] + 1).get(result[1]).getStatusSquare().equals("O")) {
+            for (int d = 0; d < 2; d++) {
+                board.ocean.get(result[0] + d).get(result[1]).setStatusSquare("S");
+            }
+        } else if (board.ocean.get(result[0]).get(result[1]).getStatusSquare().equals("O") && board.ocean.get(result[0]).get(result[1] + 1).getStatusSquare().equals("O")) {
+            for (int d = 0; d < 2; d++) {
+                board.ocean.get(result[0]).get(result[1] + d).setStatusSquare("S");
+            }
+        } else {
+            for (int d = 0; d < 2; d++) {
+                board.ocean.get(result[0] + d).get(result[1]).setStatusSquare("O");
+                board.ocean.get(result[0]).get(result[1] + d).setStatusSquare("O");
+                SubmarinePlacement(isSubmarinePlacementOk, board);
+            }
+        }
+
+        return isSubmarinePlacementOk;
+    }
+
+    public static boolean DestroyerPlacement(boolean isDestroyerPlacementOk, Board board) {
+        int[] result = BoardFactory.random(board);
+        if (board.ocean.get(result[0]).get(result[1]).getStatusSquare().equals("O")) {
+            board.ocean.get(result[0]).get(result[1]).setStatusSquare("S");
+        } else if (board.ocean.get(result[0] + 1).get(result[1] + 1).getStatusSquare().equals("O")) {
+            board.ocean.get(result[0] + 1).get(result[1] + 1).setStatusSquare("S");
+        } else {
+            board.ocean.get(result[0] + 1).get(result[1] + 1).setStatusSquare("O");
+            board.ocean.get(result[0]).get(result[1]).setStatusSquare("O");
+            DestroyerPlacement(isDestroyerPlacementOk, board);
+        }
+        return isDestroyerPlacementOk;
     }
 
 
-
-
-    private static boolean isPlacementOk(int[][] placement) {
-        return false;
-    }
 }
 
 
--- src/BoardFactory.java
+++ src/BoardFactory.java
@@ -4,6 +4,25 @@ import java.util.Random;
 //Carrier5, Cruiser3, Battleship4, Submarine2, and Destroyer1.
 public class BoardFactory {
 
+    public static void randomPlacement(Board board) {
+        int[] varResult = Board.carrierPlacement(board);
+        int right = varResult[0];
+        int down = varResult[1];
+        int result1 = varResult[2];
+        int result2 = varResult[3];
+        for(int i=0; i<5; i++) {
+            if (right == 1) {
+                board.ocean.get(result1 ).get(result2+ i).setStatusSquare("S");
+                System.out.println("działa");
+            } else if (down ==1) {
+                board.ocean.get(result1+ i ).get(result2).setStatusSquare("S");
+                System.out.println("działa w elsie");
+            }
+        }
+    }
+
+
+
 //    {
 //        draw(board);
 //        int result[] = random(board);
@@ -11,81 +30,114 @@ public class BoardFactory {
 //            board.ocean.get(result[0]).get(result[1]).setStatusSquare("S");
 //        }
 //    }
-    public static void randomPlacement(Board board) {
-        int[] resultShip = ships();
-        int[] result = random(board);
-        for(int i=0; i< result.length; i++) {
-            if (resultShip[i] == 5) {
-                for(int d=0; d<5; d++){
-                    if(board.ocean.get(result[0]).get(result[1]).getStatusSquare().equals("O") && board.ocean.get(result[0+1]).get(result[1]).getStatusSquare().equals("O")
-                            && board.ocean.get(result[0+2]).get(result[1]).getStatusSquare().equals("O") && board.ocean.get(result[0+3]).get(result[1]).getStatusSquare().equals("O")
-                            && board.ocean.get(result[0+4]).get(result[1]).getStatusSquare().equals("O")) {
-                        board.ocean.get(result[0+d]).get(result[1]).setStatusSquare("S");
-                    }else if (board.ocean.get(result[0]).get(result[1]).getStatusSquare().equals("O") && board.ocean.get(result[0]).get(result[1+1]).getStatusSquare().equals("O")
-                            && board.ocean.get(result[0]).get(result[1+2]).getStatusSquare().equals("O") && board.ocean.get(result[0]).get(result[1+3]).getStatusSquare().equals("O")
-                            && board.ocean.get(result[0]).get(result[1+4]).getStatusSquare().equals("O")){
-                        board.ocean.get(result[0]).get(result[1+d]).setStatusSquare("S");
-                    }
-                }
-            }
-            else if(resultShip[i] == 4){
-                for(int d=0; d<4; d++) {
-                    if (board.ocean.get(result[0]).get(result[1]).getStatusSquare().equals("O") && board.ocean.get(result[0+1]).get(result[1]).getStatusSquare().equals("O")
-                            && board.ocean.get(result[0+2]).get(result[1]).getStatusSquare().equals("O") && board.ocean.get(result[0+3]).get(result[1]).getStatusSquare().equals("O")) {
-                        board.ocean.get(result[0+d]).get(result[1]).setStatusSquare("S");
-                    }else if (board.ocean.get(result[0]).get(result[1]).getStatusSquare().equals("O") && board.ocean.get(result[0]).get(result[1+1]).getStatusSquare().equals("O")
-                            && board.ocean.get(result[0]).get(result[1+2]).getStatusSquare().equals("O") && board.ocean.get(result[0]).get(result[1+3]).getStatusSquare().equals("O")) {
-                        board.ocean.get(result[0]).get(result[1+d]).setStatusSquare("S");
-                    }
-                }
-            }
-            else if(resultShip[i] == 3){
-                for(int d=0; d<3; d++) {
-                    if (board.ocean.get(result[0]).get(result[1]).getStatusSquare().equals("O") && board.ocean.get(result[0+1]).get(result[1]).getStatusSquare().equals("O")
-                            && board.ocean.get(result[0+2]).get(result[1]).getStatusSquare().equals("O")) {
-                        board.ocean.get(result[0+d]).get(result[1]).setStatusSquare("S");
-                    }else if (board.ocean.get(result[0]).get(result[1]).getStatusSquare().equals("O") && board.ocean.get(result[0]).get(result[1+1]).getStatusSquare().equals("O")
-                            && board.ocean.get(result[0]).get(result[1+2]).getStatusSquare().equals("O")) {
-                        board.ocean.get(result[0]).get(result[1+d]).setStatusSquare("S");
-                    }
-                }
-            }
-            else if(resultShip[i] == 2){
-                for(int d=0; d<2; d++) {
-                    if (board.ocean.get(result[0]).get(result[1]).getStatusSquare().equals("O") && board.ocean.get(result[0+1]).get(result[1]).getStatusSquare().equals("O")) {
-                        board.ocean.get(result[0+d]).get(result[1]).setStatusSquare("S");
-                    }else if (board.ocean.get(result[0]).get(result[1]).getStatusSquare().equals("O") && board.ocean.get(result[0]).get(result[1+1]).getStatusSquare().equals("O")) {
-                        board.ocean.get(result[0]).get(result[1+d]).setStatusSquare("S");
-                    }
-                }
-            }else if(resultShip[i] == 1){
-                if (board.ocean.get(result[0]).get(result[1]).getStatusSquare().equals("O")) {
-                    board.ocean.get(result[0]).get(result[1]).setStatusSquare("S");
-                }else if (board.ocean.get(result[0+1]).get(result[1+1]).getStatusSquare().equals("O")) {
-                    board.ocean.get(result[0+1]).get(result[1+1]).setStatusSquare("S");
-                }
-            }
-        }
-    }
+//    public static void randomPlacement(Board board) {
+//        int[] resultShip = ships();
+//        for (int i = 0; i < resultShip.length; i++) {
+//            try {
+//                if (resultShip[i] == 5) {
+//                    int[] result = random(board);
+//                    if (board.ocean.get(result[0]).get(result[1]).getStatusSquare().equals("O") && board.ocean.get(result[0] + 1).get(result[1]).getStatusSquare().equals("O")
+//                            && board.ocean.get(result[0] + 2).get(result[1]).getStatusSquare().equals("O") && board.ocean.get(result[0] + 3).get(result[1]).getStatusSquare().equals("O")
+//                            && board.ocean.get(result[0] + 4).get(result[1]).getStatusSquare().equals("O")) {
+//                        for (int d = 0; d < 5; d++) {
+//                            board.ocean.get(result[0]).get(result[1] + d).setStatusSquare("S");
+//                            System.out.println("if");
+//                        }
+//                    } else if (board.ocean.get(result[0]).get(result[1]).getStatusSquare().equals("O") && board.ocean.get(result[0]).get(result[1] + 1).getStatusSquare().equals("O")
+//                            && board.ocean.get(result[0]).get(result[1] + 2).getStatusSquare().equals("O") && board.ocean.get(result[0]).get(result[1] + 3).getStatusSquare().equals("O")
+//                            && board.ocean.get(result[0]).get(result[1] + 4).getStatusSquare().equals("O")) {
+//                        for (int d = 0; d < 5; d++) {
+//                            board.ocean.get(result[0] + d).get(result[1]).setStatusSquare("S");
+//                            System.out.println("elseif");
+//                        }
+//                    } else {
+//                        System.out.println("dupa1");
+//                        resultValidation(board);
+//                    }
+//
+//
+//                } else if (resultShip[i] == 4) {
+//
+//                        int[] result = random(board);
+//                        if (board.ocean.get(result[0]).get(result[1]).getStatusSquare().equals("O") && board.ocean.get(result[0] + 1).get(result[1]).getStatusSquare().equals("O")
+//                                && board.ocean.get(result[0] + 2).get(result[1]).getStatusSquare().equals("O") && board.ocean.get(result[0] + 3).get(result[1]).getStatusSquare().equals("O")) {
+//                            for (int d = 0; d < 4; d++) {
+//                                board.ocean.get(result[0]).get(result[1] + d).setStatusSquare("S");
+//                            }
+//                        } else if (board.ocean.get(result[0]).get(result[1]).getStatusSquare().equals("O") && board.ocean.get(result[0]).get(result[1] + 1).getStatusSquare().equals("O")
+//                                && board.ocean.get(result[0]).get(result[1] + 2).getStatusSquare().equals("O") && board.ocean.get(result[0]).get(result[1] + 3).getStatusSquare().equals("O")) {
+//                            for (int d = 0; d < 4; d++) {
+//                                board.ocean.get(result[0] + d).get(result[1]).setStatusSquare("S");
+//                            }
+//                        } else {
+//                            resultValidation(board);
+//                        }
+//                    }
+
+//                else if (resultShip[i] == 3) {
+//                    for (int d = 0; d < 3; d++) {
+//                        int[] result = random(board);
+//                        if (board.ocean.get(result[0]).get(result[1]).getStatusSquare().equals("O") && board.ocean.get(result[0] + 1).get(result[1]).getStatusSquare().equals("O")
+//                                && board.ocean.get(result[0] + 2).get(result[1]).getStatusSquare().equals("O")) {
+//                            board.ocean.get(result[0] + d).get(result[1]).setStatusSquare("S");
+//                        } else if (board.ocean.get(result[0]).get(result[1]).getStatusSquare().equals("O") && board.ocean.get(result[0]).get(result[1] + 1).getStatusSquare().equals("O")
+//                                && board.ocean.get(result[0]).get(result[1] + 2).getStatusSquare().equals("O")) {
+//                            board.ocean.get(result[0]).get(result[1] + d).setStatusSquare("S");
+//                        }else{
+//                            resultValidation(board);
+//                        }
+//                    }
+//                } else if (resultShip[i] == 2) {
+//                    for (int d = 0; d < 2; d++) {
+//                        int[] result = random(board);
+//                        if (board.ocean.get(result[0]).get(result[1]).getStatusSquare().equals("O") && board.ocean.get(result[0] + 1).get(result[1]).getStatusSquare().equals("O")) {
+//                            board.ocean.get(result[0] + d).get(result[1]).setStatusSquare("S");
+//                        } else if (board.ocean.get(result[0]).get(result[1]).getStatusSquare().equals("O") && board.ocean.get(result[0]).get(result[1] + 1).getStatusSquare().equals("O")) {
+//                            board.ocean.get(result[0]).get(result[1] + d).setStatusSquare("S");
+//                        }else{
+//                            resultValidation(board);
+//                        }
+//                    }
+//                } else if (resultShip[i] == 1) {
+//                    int[] result = random(board);
+//                    if (board.ocean.get(result[0]).get(result[1]).getStatusSquare().equals("O")) {
+//                        board.ocean.get(result[0]).get(result[1]).setStatusSquare("S");
+//                    } else if (board.ocean.get(result[0] + 1).get(result[1] + 1).getStatusSquare().equals("O")) {
+//                        board.ocean.get(result[0] + 1).get(result[1] + 1).setStatusSquare("S");
+//                    }else{
+//                        resultValidation(board);
+//                    }
+//                }
+//                }catch(IndexOutOfBoundsException e){
+//                System.out.println("EXCEPT");
+//                    resultValidation(board);
+//
+//                }
+//            }
+//
+//        }
+
+
 
     public static int[] random(Board board){
         Random rand = new Random();
-        int max = board.ocean.size()-1;
-        int randomx = rand.nextInt(max);
-        int randomy = rand.nextInt(max);
+        int max = 14;
+        int randomx = rand.nextInt(9, 15);
+        int randomy = rand.nextInt(9,15);
         System.out.println(randomx);
         System.out.println(randomy);
-        return new int[] {randomx, randomy};
+        return new int[] {randomx-1, randomy-1};
     }
 
-    public static int[] ships(){
-        int Carrier = 5;
-        int Battleship = 4;
-        int Cruiser = 3;
-        int Submarine = 2;
-        int Destroyer = 1;
-        return new int[] {Carrier, Cruiser, Battleship, Submarine, Destroyer};
-    }
+//    public static int[] ships(){
+//        int Carrier = 5;
+//        int Battleship = 4;
+//        int Cruiser = 3;
+//        int Submarine = 2;
+//        int Destroyer = 1;
+//        return new int[] {Carrier, Battleship};
+//    }
+
 
 
     public static void manualPlacement(){
