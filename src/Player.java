4b206c43b397ffb900c1b046e5093eb06eb43a6e
MichalRogalski22 <michalrogalsk@gmail.com>
2022-03-09 20:00:20 +0100

    Implement handlig shots and chcecking if player have any ships left

--- src/Player.java
+++ src/Player.java
@@ -1,14 +1,34 @@
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Player {
-    public List<Ship> ship = new ArrayList<Ship>();
+    //public List<Ship> ship = new ArrayList<Ship>();
 
-    public static void handleShots(List shootingCoordinates, Object board){
+    public static void handleShots(List<Integer> shootingCoordinates, List<List<Square>> board){
+        int coordinateX = shootingCoordinates.get(0);
+        int coordinateY = shootingCoordinates.get(1);
+        switch (board.get(coordinateX).get(coordinateY).getStatusSquare()) {
+            case OCEAN:
+                board.get(coordinateX).get(coordinateY).setStatusSquare(SquareStatus.status.MISS);
+                break;
+            case SHIP:
+                board.get(coordinateX).get(coordinateY).setStatusSquare(SquareStatus.status.HIT);
+                break;
+
+        }
+        System.out.println(board.get(coordinateX).get(coordinateY).getStatusSquare());
 
     }
 
-    public static void isAlive(){
-
+    public static boolean isAlive(List<List<Square>> board){
+    for(List<Square> list : board) {
+        for(Square object : list){
+            if(object.getStatusSquare() == SquareStatus.status.SHIP){
+                return true;
+            }
+        }
+    }
+    return false;
     }
 }
