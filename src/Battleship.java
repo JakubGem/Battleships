301773bf73779c9a4e64836c49f694ede1b651a9
JakubGem <jakegemb89@gmail.com>
2022-03-10 15:02:06 +0100

    random placement is working

--- src/Battleship.java
+++ src/Battleship.java
@@ -2,16 +2,18 @@ public class Battleship {
     public static void main(String[] args) {
         Display display = new Display();
         Board board = new Board();
-        Board.carrierPlacement(board);
-        for (int i = 0; i < 2; i++) {
-            Board.battleshipPlacement(board);
-        }
-        for (int i = 0; i < 4; i++) {
-            Board.CruiserPlacement(board);
-        }
-        for (int i = 0; i < 4; i++) {
-            Board.SubmarinePlacement(board);
-        }
+//        int[] result = {2,5};
+        Board.informationToValidate(board);
+//        Board.carrierPlacement(board);
+//        for (int i = 0; i < 2; i++) {
+//            Board.battleshipPlacement(board);
+//        }
+//        for (int i = 0; i < 4; i++) {
+//            Board.CruiserPlacement(board);
+//        }
+//        for (int i = 0; i < 4; i++) {
+//            Board.SubmarinePlacement(board);
+//        }
         display.displayBoard(board);
 
     }
--- src/Board.java
+++ src/Board.java
@@ -22,29 +22,65 @@ public class Board {
     }
 
 
-    public int[] informationToValidate(){
+//    public boolean verifyRandomPlacement(int rangeHorizontal, int rangeVertical, Board board, int[] result) {
+//        boolean check = true;
+//        for (int stepHorizontal = -1; stepHorizontal < rangeHorizontal; stepHorizontal++) {
+//            for (int stepVertical = -1; stepVertical < rangeVertical; stepVertical++) {
+//                if (!board.ocean.get(result[0] - stepVertical).get(result[1] + rangeHorizontal).getStatusSquare().equals("O")) {
+//                    check = false;
+//                    break;
+//                }
+//            }
+//        }
+//
+//        if (check) {
+//            BoardFactory.randomPlacement(board, 2, 1, result);
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
 
+    public static void informationToValidate(Board board) {
+
+        int[] ships = {5, 4, 3, 3, 2};
+        for (int length : ships) {
+            int direction = randomNumber();
+            boolean check = false;
+            while (!check) {
+                try {
+                    check = verifyRandomPlacement(length, direction, board);
+                } catch (IndexOutOfBoundsException e) {
+                    continue;
+                }
+            }
+        }
     }
 
-    public boolean verifyRandomPlacement(int rangeHorizontal, int rangeVertical, Board board, int[] result) {
+    public static boolean verifyRandomPlacement(int shipLength, int direction, Board board) {
         boolean check = true;
-        for (int stepHorizontal = -1; stepHorizontal < rangeHorizontal; stepHorizontal++) {
-            for (int stepVertical = -1; stepVertical < rangeVertical; stepVertical++) {
-                if (!board.ocean.get(result[0] - stepVertical).get(result[1] + rangeHorizontal).getStatusSquare().equals("O")) {
+        int[] result = BoardFactory.random();
+        for (int step = -1; step < shipLength + 1; step++) {
+            for (int i = -1; i <= 1; i++) {
+                if (direction == 1 && !board.ocean.get(result[0] + i).get(result[1] + step).getStatusSquare().equals("O")) {
+                    check = false;
+                    break;
+                } else if (direction == 2 && !board.ocean.get(result[0] + step).get(result[1] + i).getStatusSquare().equals("O")) {
                     check = false;
                     break;
                 }
             }
+            if (!check) {
+                break;
+            }
         }
-
         if (check) {
-            BoardFactory.randomPlacement(board, 2, 1, result);
+            BoardFactory.randomPlacement(board, shipLength, direction, result);
             return true;
         } else {
             return false;
         }
     }
-
 //    public static void carrierPlacement(Board board) {
 //        //5
 //        int[] result = BoardFactory.random();
@@ -114,7 +150,6 @@ public class Board {
 //            battleshipPlacement(board);
 //        }
 //    }
-
 
 
 //
@@ -216,7 +251,7 @@ public class Board {
 
     public static int randomNumber() {
         Random rand = new Random();
-        int randomNumber = rand.nextInt(1, 2);
+        int randomNumber = rand.nextInt(1, 3);
         return randomNumber;
     }
 }
